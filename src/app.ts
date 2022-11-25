9430de3b104a9023eedd959034d990dc8de8b8b7
lanka101 <lahiru.rathnayaka@101digital.io>
2022-11-25 17:16:32 +0530

    adding schemas

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,12 @@ import express from "express";
 import config from "config";
 import log from "./utils/logger";
 import connectDb from "./utils/DbConnection";
+import router from "./routes";
 
 const app = express();
-
 app.use(express.json());
+app.use(router);
+
 const port = config.get("port");
 app.listen(port, () => {
   log.info(`App started at http://localhost:${port}`);
